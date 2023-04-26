1cd18f1bf095748da4ac5ada89977d39cb8bb1d1
Joselois1 <sfxjose@gmail.com>
2023-04-26 10:32:55 -0400

    implementacion distintas busquedas

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # t1ia
 Trabajo 1 inteligancia artificial
 
-Cambio al que debo hacer commit
+Para ejecutar el codigo una vez descargado se ejecuta el comando 
+python3 main.py
+
+Se imprimira en pantalla una lista de adyacencia la cual define el grafo con el que estamos trabajando.
+
+luego se nos pedira el tipo de busqueda que podemos realizar entre los que estan:
+
+Busqueda Profundidad (dps)
+Busqueda Greedy (grd)
+Busqueda costo uniforme (ucs)
+A estrella (a)
+
+Ingresa la abreviacion despues del mensaje y recibe el camino encontrado por la busqueda y el costo de la misma
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import heapq
+
 lista_ady = {}
 heuristica = {}
 with open('grafo.txt', 'r') as f:
@@ -35,30 +37,103 @@ for linea in lineas[10:len(lineas)-1]:
     lista_ady[origen].append((destino,costo))     
     #print(lista_ady["G"])
 
-#for node in lista_ady:
-#    print(f"{node}: {lista_ady[node]}")
+for node in lista_ady:
+    print(f"{node}: {lista_ady[node]}")
 
 
-def dfs_recursiva(lista_ady, nodo_inicial, visited = None):
+def busqueda_profundidad_recursiva(lista_ady, nodo_inicial, visited = None):
     costo = 0
     if visited is None:
         visited = set()
-
     visited.add(nodo_inicial)
-
     print(f"{nodo_inicial}->", end=" ")
+    if nodo_inicial == nodo_objetivo:
+        return True
     
         
     for neighbor in lista_ady[nodo_inicial]:
         if neighbor not in visited:
-            costo = costo + int(heuristica[nodo_inicial])   
-            dfs_recursiva(lista_ady, neighbor[0])
-        if neighbor := nodo_objetivo:
-                break 
-       
+            costo = costo + int(heuristica[nodo_inicial])  
+            busqueda_profundidad_recursiva(lista_ady, neighbor[0])
+        if busqueda_profundidad_recursiva(lista_ady, neighbor[0], visited):
+            return True
+    return False
+
+#def busqueda_costo_uniforme(lista_ady, nodo_inicial, nodo_objetivo):
+#    cola_p = [(0,nodo_inicial)]
+#    costo = {nodo_inicial: 0}
+#    padre ={nodo_inicial: None}
+#    while cola_p: 
+#        (costo, nodo_actual) = heapq.heappop(cola_p)
+#        if nodo_inicial == nodo_objetivo:
+#            camino = []
+#            while nodo_inicial is not None:
+#                camino.append(nodo_inicial)
+#                nodo_inicial = padre[nodo_inicial]                
+#            print(nodo_inicial)
+
+def busqueda_costo_uniforme(graph, start, goal):
+    visited = set()
+    queue = [(0, start, [])]
+    
+    while queue:
+        (cost, node, path) = heapq.heappop(queue)
+        
+        if node not in visited:
+            visited.add(node)
+            path = path + [node]
+            
+            if node == goal:
+                return path
+            
+            for (neighbor, weight) in graph[node]:
+                if neighbor not in visited:
+                    heapq.heappush(queue, (cost + weight, neighbor, path))
+                 
+    return None
+
+
+def busqueda_greedy(graph, start, goal):
+    visited = set()
+    queue = [(heuristica[start], start, [])] # Use heuristic value as priority
+    
+    while queue:
+        (_, node, path) = heapq.heappop(queue)
+        
+        if node not in visited:
+            visited.add(node)
+            path = path + [node]
+            
+            if node == goal:
+                return path
+            
+            for (neighbor, weight) in graph[node]:
+                if neighbor not in visited:
+                    heapq.heappush(queue, (heuristica[neighbor], neighbor, path))                
+    return None
 
 
 
 
-dfs_recursiva(lista_ady, nodo_inicial)
 
+print("Busqueda Profundidad (dps)")
+print("Busqueda Greedy (grd)")
+print("Busqueda costo uniforme (ucs)")
+print("A estrella (a))")
+tipo = input("Ingrese el tipo de busqueda que desea realizar: ")
+
+if tipo == "dps":
+    busqueda_profundidad_recursiva(lista_ady, nodo_inicial)
+if tipo == "grd":
+    path = busqueda_greedy(lista_ady ,nodo_inicial, nodo_objetivo)
+    for nodo in path:
+        print(f"{nodo}-> ", end = " ")
+if tipo == "ucs":
+    path = busqueda_costo_uniforme(lista_ady  ,nodo_inicial, nodo_objetivo)
+    for nodo in path:
+        print(f"{nodo}-> ", end = " ")
+if tipo == "a":
+    path = busqueda_profundidad_recursiva(lista_ady, nodo_inicial)
+
+#busqueda_costo_uniforme(lista_ady  ,nodo_inicial, nodo_objetivo)
+#busqueda_profundidad_recursiva(lista_ady, nodo_inicial)
